19a19db79299e57aca3130dc00115a54c6a09398
Sirisha Guntur <sirisha.guntur@B-1292.local>
2020-05-19 17:57:56 +0100

    Added git ignore

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello Babylon! Welcome to my first jenkins example");
-        System.out.println("Here are the new changes")
+        System.out.println("Here are the new changes");
     }
 }
