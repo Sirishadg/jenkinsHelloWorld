3daad996c96f2a84eceeef04d379b1036863c797
Sirishadg <50921880+Sirishadg@users.noreply.github.com>
2020-05-20 21:33:30 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello Babylon! Welcome to my first jenkins example");
-        System.out.println("Here are the new changes");
     }
 }
